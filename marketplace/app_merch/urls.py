13e1260612526be9c8b54b4631117ba78d5f5e6a
Konst24 <darthtogors@gmail.com>
2023-03-10 16:22:23 +0300

    исправления в static

--- marketplace/app_merch/urls.py
+++ marketplace/app_merch/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from app_merch.views import TestView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', TestView),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
