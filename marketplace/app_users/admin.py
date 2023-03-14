e358eff50df10456adddc10cbbb4e0c865753afd
maksim komovich <maxim.komovich@gmail.com>
2023-03-14 15:51:53 +0300

    Созданы модели профиля и продавца и добавлены в интерфейс администратора.

--- marketplace/app_users/admin.py
+++ marketplace/app_users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from app_users.models import Profile, Saller
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name', ]
+    search_fields = ['full_name', ]
+
+
+@admin.register(Saller)
+class SallerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', ]
+    search_fields = ['title', ]
+
