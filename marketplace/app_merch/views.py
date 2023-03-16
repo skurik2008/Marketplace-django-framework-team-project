e384e379f318ae0aabf02f56db5db52c3306339b
Maksim Strelchenko <bender404123@gmail.com>
2023-03-16 21:30:12 +0300

    Добавлена главная страница и отображение 3х случайных баннеров

--- marketplace/app_merch/views.py
+++ marketplace/app_merch/views.py
@@ -1,7 +1,31 @@
 from django.views.generic import ListView
-from .models import Category
+from .models import Category, Product, Banner
 from app_settings.models import SiteSettings
 from django.core.cache import cache
+
+
+class IndexView(ListView):
+    """ Вью класс для главной страницы MEGANO. """
+    template_name = 'index.html'
+    context_object_name = 'products'
+    model = Product
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        time_to_cache = SiteSettings.load().time_to_cache
+
+        if not time_to_cache:
+            time_to_cache = 1
+
+        context['categories'] = cache.get_or_set(
+            'Categories',
+            Category.objects.filter(is_active=True),
+            time_to_cache * 60 * 60
+        )
+
+        context['banners'] = Banner.objects.order_by('?')[:3]
+
+        return context
 
 
 class CategoryView(ListView):
