9e3e3eac29f9f13528e91cc93fb4745627d07fe9
Maksim Strelchenko <bender404123@gmail.com>
2023-03-18 12:49:11 +0300

    Добавил тег для получения категорий. Удалил комментарий с базового шаблона. Изменил логику получения категорий в header. Удалил лишний контекст из вью главной страницы

--- marketplace/app_merch/views.py
+++ marketplace/app_merch/views.py
@@ -12,20 +12,10 @@ class IndexView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
-        time_to_cache = SiteSettings.load().time_to_cache
         banners_cache_time = SiteSettings.load().banners_cache_time
-
-        if not time_to_cache:
-            time_to_cache = 1
 
         if not banners_cache_time:
             banners_cache_time = 10
-
-        context['categories'] = cache.get_or_set(
-            'Categories',
-            Category.objects.filter(is_active=True),
-            time_to_cache * 60 * 60 * 24
-        )
 
         context['banners'] = cache.get_or_set(
             'Banners',
