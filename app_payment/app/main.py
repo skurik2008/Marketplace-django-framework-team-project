550fd043ed8019f3315e4facec8356481a6630ee
Maksim Strelchenko <bender404123@gmail.com>
2023-04-01 15:26:19 +0300

    Добавлен сервис оплаты заказа, используется celery

---
+++ app_payment/app/main.py
@@ -0,0 +1,59 @@
+from random import choice
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from schemas import (
+    PaymentInformation
+)
+
+app = FastAPI()
+
+PAYMENT_ERRORS: tuple = (
+    'Not enough money',
+    'Card declined',
+    'Service is unreachable',
+    'Bank not supported'
+)
+
+
+async def validate_card_number(card_number: str) -> bool:
+    """ Функция валидации карты. """
+    user_card_number: str = card_number.replace(' ', '')
+    if len(user_card_number) == 8:
+        try:
+            int_card_number: int = int(user_card_number)
+        except ValueError:
+            return False
+        else:
+            if int_card_number % 2 == 0 and user_card_number[-1] != '0':
+                return True
+    return False
+
+
+@app.post('/api/v1/purchase/')
+async def make_payment(
+        payment_information: PaymentInformation
+) -> JSONResponse:
+    """
+    Эндпоинт для проведения оплаты.
+    Если карта не проходит валидацию, платёж отклоняется со случайной ошибкой.
+    """
+    if not await validate_card_number(payment_information.card_number):
+        return JSONResponse(
+            status_code=403,
+            content={
+                "status": "error",
+                "message": choice(PAYMENT_ERRORS)
+            }
+        )
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "status": "success",
+            "message": "Order {} was successfully purchased.".format(
+                payment_information.order_id
+            )
+        }
+    )
---
+++ app_payment/app/schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class PaymentInformation(BaseModel):
+    """ Схема информации о пользователе. """
+    username: str
+    order_id: int
+    card_number: str
+    amount: float
