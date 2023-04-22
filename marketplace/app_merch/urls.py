43bc020ac40937885529408f1a27c5b6f8662553
Maksim Strelchenko <maksim.strelchenko99@gmail.com>
2023-04-22 13:09:01 +0300

    Добавлены страницы ввода информации для формирования заказа. Добавлены картинки-заглушки

--- marketplace/app_merch/urls.py
+++ marketplace/app_merch/urls.py
@@ -2,8 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from .views import (CatalogView, IndexView, ProductDetailView,
-                    ProductPurchaseView, OrderView)
+from .views import (
+    CatalogView,
+    IndexView,
+    ProductDetailView,
+    ProductPurchaseView,
+    OrderUserDataView,
+    OrderDeliveryView,
+    OrderPaymentView,
+    OrderPurchaseView
+)
 
 app_name = 'pages'
 
@@ -12,5 +20,8 @@ urlpatterns = [
     path('products/', CatalogView.as_view(), name='catalog-view'),
     path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('product_detail/<int:pk>/purchase/', ProductPurchaseView.as_view(), name='offer-purchase'),
-    path('order/', OrderView.as_view(), name='order-view'),
+    path('order/userdata/', OrderUserDataView.as_view(), name='order-step-1'),
+    path('order/delivery/', OrderDeliveryView.as_view(), name='order-step-2'),
+    path('order/payment/', OrderPaymentView.as_view(), name='order-step-3'),
+    path('order/purchase/', OrderPurchaseView.as_view(), name='order-step-4'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
